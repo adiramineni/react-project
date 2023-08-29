e69c4c20abe64483a082d2d78ea85aa8cb4bd3e1
adiramineni <adhiramineni@gmail.com>
2023-08-29 21:40:10 +0530

    first commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,15 @@ import Card from '../UI/card.js';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>
-        </div>
+            <button onClick={() => {console.log('Clicked!')}}>Change Title</button>
+        </Card>
     );
 }
 
